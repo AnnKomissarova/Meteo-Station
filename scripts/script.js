6944525cc5c8bb6c1048b23b7f9ceb044360f167
AnnKomissarova <nnkmssrv@gmail.com>
2023-03-12 20:28:09 +0300

    Adding-modal

--- scripts/script.js
+++ scripts/script.js
@@ -14,3 +14,26 @@ function tabClick(event) {
   tabs[tabId - 1].classList.add('active');
   sections[tabId - 1].classList.add('active');
 }
+
+const modal = document.querySelector(".modal");
+const bodyOverlay = document.querySelector(".body-overlay");
+const body = document.querySelector("body");
+const openModalBtn = document.querySelector("#modal-menu");
+const btnClose = document.querySelector(".btn-text-secondary");
+const btnOK = document.querySelector(".btn-text-primary")
+
+function openModal() {
+  modal.classList.add('active');
+  bodyOverlay.classList.add('active');
+  body.classList.add('with-overlay');
+};
+
+function closeModal() {
+  modal.classList.remove('active');
+  bodyOverlay.classList.remove('active');
+  body.classList.remove('with-overlay');
+};
+
+openModalBtn.addEventListener('click', openModal);
+btnClose.addEventListener('click', closeModal);
+btnOK.addEventListener('click', closeModal);
